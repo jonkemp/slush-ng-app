441a0f4a03bdf7a4f964d8de5bc343998818f0ac
Jonathan Kemp <kempdogg@gmail.com>
2014-07-04 13:47:22 -0400

    Listen to 'finish' event to call 'done'.

--- slushfile.js
+++ slushfile.js
@@ -60,7 +60,7 @@ gulp.task('default', function (done) {
             .pipe(conflict('./'))
             .pipe(gulp.dest('./'))
             .pipe(install())
-            .on('end', function () {
+            .on('finish', function () {
                 done();
             });
     });
