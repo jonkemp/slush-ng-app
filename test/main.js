18642b34a3b6966030c63d85ac9b455d0b1a8898
Jonathan Kemp <kempdogg@gmail.com>
2014-07-04 13:46:42 -0400

    Add tests.

---
+++ test/main.js
@@ -0,0 +1,84 @@
+/* jshint node:true */
+/* global require, it, before, beforeEach, describe */
+
+'use strict';
+var should = require('should'),
+    inquirer = require('inquirer'),
+    gulp = require('gulp'),
+    mockGulpDest = require('mock-gulp-dest')(gulp);
+
+require('../slushfile');
+
+/**
+ * Mock inquirer prompt
+ */
+
+function mockPrompt(answers) {
+    inquirer.prompt = function (prompts, done) {
+
+        [].concat(prompts).forEach(function (prompt) {
+            if (!(prompt.name in answers)) {
+                answers[prompt.name] = prompt.default;
+            }
+        });
+
+        done(answers);
+    };
+}
+
+describe('slush-ng-app', function() {
+    before(function () {
+        process.chdir(__dirname);
+    });
+
+    describe('default generator', function () {
+        beforeEach(function () {
+            mockPrompt({
+                moveon: true
+            });
+        });
+
+        it('should put all project files in current working directory', function (done) {
+            gulp.start('default').once('stop', function () {
+                mockGulpDest.cwd().should.equal(__dirname);
+                mockGulpDest.basePath().should.equal(__dirname);
+                done();
+            });
+        });
+
+        it('should create expected files', function (done) {
+            gulp.start('default').once('stop', function () {
+                mockGulpDest.assertDestContains([
+                    '.bowerrc',
+                    '.gitignore',
+                    '.jshintrc',
+                    '.travis.yml',
+                    'bower.json',
+                    'package.json',
+                    'LICENSE',
+                    'README.md',
+                    'app/index.html',
+                    'app/index-async.html',
+                    'app/img/.gitkeep',
+                    'app/css/app.css',
+                    'app/js/app.js',
+                    'app/js/controllers.js',
+                    'app/js/directives.js',
+                    'app/js/filters.js',
+                    'app/js/services.js',
+                    'app/partials/partial1.html',
+                    'app/partials/partial2.html',
+                    'test/karma.conf.js',
+                    'test/protractor-conf.js',
+                    'test/e2e/scenarios.js',
+                    'test/unit/controllersSpec.js',
+                    'test/unit/directivesSpec.js',
+                    'test/unit/filtersSpec.js',
+                    'test/unit/servicesSpec.js'
+                ]);
+
+                done();
+            });
+        });
+    });
+});
