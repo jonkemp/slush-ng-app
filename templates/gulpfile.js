e3a28561090f7e93ff63c5b37256f59715447bae
Jonathan Kemp <jonathan.kemp@cellularsales.com>
2014-07-24 15:45:29 -0400

    Convert gulpfile to 2 spaces.

--- templates/gulpfile.js
+++ templates/gulpfile.js
@@ -3,82 +3,82 @@
 var gulp = require('gulp');
 
 gulp.task('clean', function(cb) {
-    require('rimraf')('dist', cb);
+  require('rimraf')('dist', cb);
 });
 
 gulp.task('lint', function() {
-    var jshint = require('gulp-jshint');
+  var jshint = require('gulp-jshint');
 
-    return gulp.src('app/js/**/*.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'));
+  return gulp.src('app/js/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
 });
 
 gulp.task('images', function(){
-    return gulp.src('app/img/{,*/}*.{png,jpg,jpeg,gif,webp,svg}')
-        .pipe(gulp.dest('dist/img'));
+  return gulp.src('app/img/{,*/}*.{png,jpg,jpeg,gif,webp,svg}')
+    .pipe(gulp.dest('dist/img'));
 });
 
 gulp.task('html', function(){
-    var uglify = require('gulp-uglify'),
-        minifyCss = require('gulp-minify-css'),
-        useref = require('gulp-useref'),
-        gulpif = require('gulp-if');
+  var uglify = require('gulp-uglify'),
+    minifyCss = require('gulp-minify-css'),
+    useref = require('gulp-useref'),
+    gulpif = require('gulp-if');
 
-    return gulp.src('app/*.html')
-        .pipe(useref.assets())
-        .pipe(gulpif('*.js', uglify()))
-        .pipe(gulpif('*.css', minifyCss()))
-        .pipe(useref.restore())
-        .pipe(useref())
-        .pipe(gulp.dest('dist'));
+  return gulp.src('app/*.html')
+    .pipe(useref.assets())
+    .pipe(gulpif('*.js', uglify()))
+    .pipe(gulpif('*.css', minifyCss()))
+    .pipe(useref.restore())
+    .pipe(useref())
+    .pipe(gulp.dest('dist'));
 });
 
 // inject bower components
 gulp.task('wiredep', function () {
-    var wiredep = require('wiredep').stream;
+  var wiredep = require('wiredep').stream;
 
-    gulp.src('app/*.html')
-        .pipe(wiredep({
-            directory: 'app/bower_components'
-        }))
-        .pipe(gulp.dest('app'));
+  gulp.src('app/*.html')
+    .pipe(wiredep({
+      directory: 'app/bower_components'
+    }))
+    .pipe(gulp.dest('app'));
 });
 
 gulp.task('connect', function() {
-    var connect = require('connect');
-    var app = connect()
-        .use(require('connect-livereload')({ port: 35729 }))
-        .use(connect.static('app'))
-        .use(connect.directory('app'));
+  var connect = require('connect');
+  var app = connect()
+    .use(require('connect-livereload')({ port: 35729 }))
+    .use(connect.static('app'))
+    .use(connect.directory('app'));
 
-    require('http').createServer(app)
-        .listen(9000)
-        .on('listening', function() {
-            console.log('Started connect web server on http://localhost:9000');
+  require('http').createServer(app)
+    .listen(9000)
+    .on('listening', function() {
+      console.log('Started connect web server on http://localhost:9000');
 
-            require('opn')('http://localhost:9000');
-        });
+      require('opn')('http://localhost:9000');
+    });
 });
 
 gulp.task('serve', ['connect'], function () {
-    var livereload = require('gulp-livereload');
+  var livereload = require('gulp-livereload');
 
-    livereload.listen();
+  livereload.listen();
 
-    // watch for changes
-    gulp.watch([
-        'app/*.html',
-        'app/css/**/*.css',
-        'app/js/**/*.js',
-        'app/img/**/*'
-    ]).on('change', livereload.changed);
+  // watch for changes
+  gulp.watch([
+    'app/*.html',
+    'app/css/**/*.css',
+    'app/js/**/*.js',
+    'app/img/**/*'
+  ]).on('change', livereload.changed);
 
-    gulp.watch('bower.json', ['wiredep']);
+  gulp.watch('bower.json', ['wiredep']);
 });
 
 gulp.task('build', ['lint', 'html', 'images']);
 
 gulp.task('default', ['clean'], function () {
-    gulp.start('build');
+  gulp.start('build');
 });
