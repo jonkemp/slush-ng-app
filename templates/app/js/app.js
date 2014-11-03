b10ab21fbb753f2b6069722f5c06e9bbdb7303f0
Kemp, Jonathan <jkemp@scrippsnetworks.com>
2014-11-03 14:42:56 -0500

    Update templates to match latest angular-seed.

--- templates/app/js/app.js
+++ templates/app/js/app.js
@@ -1,16 +1,12 @@
 'use strict';
 
-
-// Declare app level module which depends on filters, and services
+// Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
+  'myApp.view1',
+  'myApp.view2',
+  'myApp.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
